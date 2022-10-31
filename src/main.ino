8583a25c03149cdddbeeedd178b2bbe3309f8ff3
Softwarecrash <lacrimosa2k2@gmail.com>
2022-10-31 14:50:32 +0100

    changes

--- src/main.ino
+++ src/main.ino
@@ -1,8 +1,7 @@
-//https://wokwi.com/projects/346341081902744147
+// https://wokwi.com/projects/346341081902744147
 
-
-//das noch einbauen
-//https://github.com/thomasfredericks/Bounce2
+// das noch einbauen
+// https://github.com/thomasfredericks/Bounce2
 
 /*
   TODO:
@@ -21,6 +20,7 @@
 #include <ESPAsyncWiFiManager.h>
 #include <ESPAsyncTCP.h>
 #include <ESPAsyncWebServer.h>
+#include <FastLED.h>
 
 #include "Settings.h"
 
@@ -34,34 +34,35 @@
 #define sensorIn_2 D6 // Pin of the second sensor when entering the room
 #define buzzerOut D7  // Pin for the buzzer that can make *BEEP BOOP BEEP*
 #define bellOut D8    // Pin for external switch or relay that will ring the big bell
+#define ledPin D0     // pin for ws2812 rgb stripe
 
 #define sensorState_1 false // idle state of sensor 1
 #define sensorState_2 false // idle state of sensor 2
 #define bellOutState false  // idle state of pinout external switch
+#define amount_led 8
 
-long unsigned int coolDownTime = 2000; //time after bell rings, to get back to detection
-long unsigned int bellSignalTime = 1500; //signal duration for bell pin
-long unsigned int signalTimeout = 1000; //time after one pin is triggered to get back to detection
+CRGB leds[amount_led];
 
-#define stateLedR D4
-#define stateLedG D3
-#define stateLedB D2
+long unsigned int coolDownTime = 2000;   // time after bell rings, to get back to detection
+long unsigned int bellSignalTime = 1500; // signal duration for bell pin
+long unsigned int signalTimeout = 1000;  // time after one pin is triggered to get back to detection
 
 //------------------------------------------------------------------------
 
 typedef enum
 {
-  IDLE,   //idle, wait for signal
-  PIN,    //Possibil a person going in, first sensor triggered frist
-  RING,   //first sensor triggered, second sensor triggered, make a ring
-  POUT,   //Possibil a person going out, second sensor triggered first
-  COOLDOWN //after action, let cooldown
+  IDLE,                 // idle, wait for signal
+  IN,                   // Possibil a person going in, first sensor triggered frist
+  RING,                 // first sensor triggered, second sensor triggered, make a ring
+  OUT,                  // Possibil a person going out, second sensor triggered first
+  COOLDOWN              // after action, let cooldown
 } stateList;            // list of all states
 stateList state = IDLE; // set the initial state
 bool sensor1;
 bool sensor2;
 bool buzzer;
 bool bell;
+byte ledChange; // switch for changed led data
 int amountIn;
 int amountOut;
 long unsigned int lastStateMillis;
@@ -74,12 +75,25 @@ void setup()
   pinMode(buzzerOut, OUTPUT);
   pinMode(bellOut, OUTPUT);
 
-  pinMode(stateLedR, OUTPUT);
-  pinMode(stateLedG, OUTPUT);
-  pinMode(stateLedB, OUTPUT);
+  FastLED.addLeds<WS2812B, ledPin, GRB>(leds, amount_led);
+  //-----------------------------------------------------------------------------------
+  for (size_t i = 600; i < 850; i++)
+  {
+    tone(buzzerOut, i);
+    delay(1);
+  }
+  noTone(buzzerOut);
 
-  tone(buzzerOut, 55, 500); //https://github.com/connornishijima/arduino-volume1
   Serial.println("Setup Complete... Start watching");
+
+  leds[0] = CRGB::BlueViolet;
+  leds[1] = CRGB::BlueViolet;
+  leds[3] = CRGB::BlueViolet;
+  leds[4] = CRGB::BlueViolet;
+  leds[6] = CRGB::BlueViolet;
+  leds[7] = CRGB::BlueViolet;
+  FastLED.show();
+  FastLED.delay(10);
 }
 
 void loop()
@@ -88,98 +102,119 @@ void loop()
   sensor2 = digitalRead(sensorIn_2);
   digitalWrite(bellOut, bell);
   stateRing();
+  stateLED();
 }
 
 void stateRing()
 {
-switch (state)
+  switch (state)
   {
-    case IDLE:
-      stateLED(0, 255, 0);
-      if (sensor1 != sensorState_1)
-      {
-        state = PIN;
-        lastStateMillis = millis();
-      }
-      else if (sensor2 != sensorState_2)
-      {
-        state = POUT;
-        lastStateMillis = millis();
-      }
-      break;
+  case IDLE:
+    if (sensor1 != sensorState_1)
+    {
+      state = IN;
+      lastStateMillis = millis();
+    }
+    else if (sensor2 != sensorState_2)
+    {
+      state = OUT;
+      lastStateMillis = millis();
+    }
+    break;
 
-    case PIN:
-      stateLED(155, 255, 0);
-      if (sensor2 != sensorState_2)
-      {
-        state = RING;
-        lastStateMillis = millis();
-      }
-      else if (millis() >= (lastStateMillis + signalTimeout))
-      {
-        state = IDLE;
-      }
-      break;
+  case IN:
+    if (sensor2 != sensorState_2)
+    {
+      state = RING;
+      lastStateMillis = millis();
+    }
+    else if (millis() >= (lastStateMillis + signalTimeout))
+    {
+      state = IDLE;
+    }
+    break;
 
-    case RING:
-      stateLED(255, 0, 0);
-      bell = true;
-      if (millis() >= (lastStateMillis + bellSignalTime))
-      {
-        bell = false;
-        amountIn++;
-        serialState("Objects going in: "+String(amountIn));
-        state = COOLDOWN;
-        lastStateMillis = millis();
-      }
-      break;
+  case RING:
+    bell = true;
+    if (millis() >= (lastStateMillis + bellSignalTime))
+    {
+      bell = false;
+      amountIn++;
+      serialState("Objects going in: " + String(amountIn));
+      state = COOLDOWN;
+      lastStateMillis = millis();
+    }
+    break;
 
-    case POUT:
-    //hier haut noch nicht ganz das hin was soll
-      stateLED(0, 255, 255);
-      if (sensor1 != sensorState_1)
-      {
-        amountOut++;
-        serialState("Objects going out: "+String(amountOut));
-        state = COOLDOWN;
-      }
-      else if (millis() >= (lastStateMillis + signalTimeout))
-      {
-        state = IDLE;
-      }
-      break;
+  case OUT:
+    // hier haut noch nicht ganz das hin was soll
+    if (sensor1 != sensorState_1)
+    {
+      amountOut++;
+      serialState("Objects going out: " + String(amountOut));
+      state = COOLDOWN;
+    }
+    else if (millis() >= (lastStateMillis + signalTimeout))
+    {
+      state = IDLE;
+    }
+    break;
 
-    case COOLDOWN:
-      stateLED(0, 0, 255);
-      if (millis() >= (lastStateMillis + coolDownTime))
-      {
-        serialState("Objects inside: "+String((amountIn-amountOut)));
-        state = IDLE;
-      }
-      else if (sensor1 != sensorState_1 || sensor2 != sensorState_2)
-      {
-        lastStateMillis = millis();
-      }
-      break;
+  case COOLDOWN:
+    if (millis() >= (lastStateMillis + coolDownTime))
+    {
+      serialState("Objects inside: " + String((amountIn - amountOut)));
+      state = IDLE;
+    }
+    else if (sensor1 != sensorState_1 || sensor2 != sensorState_2)
+    {
+      lastStateMillis = millis();
+    }
+    break;
 
-    default:
-      break;
+  default:
+    break;
   }
 }
 
-void stateLED(int r, int g, int b)
+void stateLED()
 {
-  analogWrite(stateLedR, r);
-  analogWrite(stateLedG, g);
-  analogWrite(stateLedB, b);
+  if (state != ledChange)
+  {
+    ledChange = state; // set the current state to change
+
+    FastLED.clear(true);
+    switch (state)
+    {
+    case COOLDOWN:
+      for (size_t i = 0; i < amount_led; i++)
+      {
+        leds[i] = CRGB::Blue;
+      }
+      break;
+    case IDLE:
+      for (size_t i = 0; i < amount_led; i++)
+      {
+        leds[i] = CRGB::Green;
+      }
+      break;
+    case IN:
+      leds[0] = CRGB::Red;
+      break;
+    case OUT:
+      leds[7] = CRGB::Yellow;
+      break;
+    }
+    FastLED.show();
+  }
 }
 
 void serialState(String message)
 {
   String tmpMessage;
-  if(tmpMessage != message)
+  if (tmpMessage != message)
   {
-  Serial.println(message);
-  tmpMessage = message;
+    Serial.println(message);
+    tmpMessage = message;
   }
 }
